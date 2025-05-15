77e9d704c811788908a63f2ef623b688a287615b
macobia <cmacobia@gmail.com>
2025-05-15 15:30:59 +0100

    fixed deployment issues by downgrading express

--- test.js
+++ test.js
@@ -375,6 +375,61 @@
 // 	}
 // };
 
+// import express from "express";
+// import dotenv from "dotenv";
+// import cookieParser from "cookie-parser";
+// import path from "path";
+
+// import authRoutes from "./routes/auth.route.js";
+// import productRoutes from "./routes/product.route.js";
+// import cartRoutes from "./routes/cart.route.js";
+// import couponRoutes from "./routes/coupon.route.js";
+// import paymentRoutes from "./routes/payment.route.js";
+// import analyticsRoutes from "./routes/analytics.route.js";
+// import cors from "cors";
+
+// import { connectDB } from "./lib/db.js";
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(express.json({ limit: '50mb' }));
+// app.use(cors({
+//     origin: process.env.CLIENT_URL || 'http://localhost:5173', // frontend URL
+//     credentials: true 
+//   }));
+// // app.use(cors())
+// const port = process.env.PORT || 3000
+// const __dirname = path.resolve(); // Get the current directory path as an absolute path 
+
+// // serve frontend
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// 	app.get("/*", (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+// 	});
+// }
+
+// // app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(cookieParser());
+
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/products", productRoutes);
+// app.use("/api/cart", cartRoutes);
+// app.use("/api/coupons", couponRoutes);
+// app.use("/api/payments", paymentRoutes);
+// app.use("/api/analytics", analyticsRoutes);
+
+
+// app.listen(port, () => {
+//     console.log("server is running on http://localhost:" + port);
+    
+//     connectDB()
+// } )
 
 const startDate = new Date();
 console.log(startDate);
