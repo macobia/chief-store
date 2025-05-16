7bb5b0aa847ae19eac03ad77e2cfefb3412e43a1
macobia <cmacobia@gmail.com>
2025-05-16 08:35:58 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -42,14 +42,14 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-// if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 
-// }
+}
 
 app.listen(port, () => {
     console.log("server is running on http://localhost:" + port);
