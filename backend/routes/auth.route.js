01d9f0fe96c19df93b1e789222fd61319b4f1f4d
macobia <cmacobia@gmail.com>
2025-05-18 11:21:13 +0100

    .

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/refreshToken", refreshToken);
+router.post("/refresh-token", refreshToken);
 router.post("/forgot-password", forgotPassword); 
 router.post("/reset-password/:token", resetPassword);          
 
