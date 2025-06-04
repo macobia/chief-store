19e3d02eaa01ccf3606fcd2a34c78f410948c075
macobia <cmacobia@gmail.com>
2025-06-04 21:17:32 +0100

    fixed bug

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: MongoStore.create({
-    mongoUrl: process.env.MONGO_URI,
+    mongoUrl: process.env.MONGO_URL,
     collectionName: "sessions",
   }),
   cookie: {
